522f2de9e854b5f466a1bf930b81ee3e7d72142b
teddykopudo <teddykopudo@gmail.com>
2023-06-15 14:28:44 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,11 @@
 #include <stdio.h>
 /**
  * main - prints the line "programming is like building a multilingual puzzle"
+ *
  * Return:0 always, non-zero otherwise
  */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzlle");
-return(0);
+	puts("\"Programming is like building a multilingual puzlle");
+	return (0);
 }
