d09fee4ff94d80d0a857ee63c39561b6f59016ee
teddykopudo <teddykopudo@gmail.com>
2023-06-15 14:50:26 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
- * main - prints "with proper grammar, but the outcome is a piece of art,
+ * main - prints "with proper grammar, but the outcome is a piece of art,"
  *
  * Return (0)
  */
