3b54211ae6d3e9aa15413ea42f76f34d9876ce7c
teddykopudo <teddykopudo@gmail.com>
2023-06-15 15:15:58 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 /**
- * main - prints the size of various types
- *
- * return: 0 if exited properly, non-zero otherwise
+ *main - prints the size of various types
+ *return: 0 if exited properly, non-zero otherwise
  */
 int main(void)
 {
