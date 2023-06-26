0a6e49f24a1f14e0136b1da0083189732d2051dd
teddykopudo <teddykopudo@gmail.com>
2023-06-26 14:27:55 -0700

    2-strlen.c

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -3,12 +3,14 @@
 /**
  * _strlen - returns the length of a string
  * @s: input
+ *
  * Return: length of string
  */
 
 int _strlen(char *s)
 {
 	int count = 0;
+
 	while (*(s + count) != '\0')
 		count++;
 	return (count);
