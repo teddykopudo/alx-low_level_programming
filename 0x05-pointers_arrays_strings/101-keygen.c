ba4503dc741b2d6d4119c19b15460588d554635d
teddykopudo <teddykopudo@gmail.com>
2023-06-26 15:39:53 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -14,10 +14,17 @@ int main(void)
 	while (c < 2772)
 	{
 		r = rand() % 128;
-		if ((c + r) > 2772)
-			break;
+		
 		c = c + r;
-		printf("%c", r);
+
+		if (c == 0)
+		{
+			printf(" ");
+		}
+		else
+		{
+			printf("%c", r);
+		}
 	}
 	printf("%c\n", (2772 - c));
 	return (0);
