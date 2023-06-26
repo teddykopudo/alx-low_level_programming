05b78ba0c036e1eeea55e8bcb64280df5b4b5963
teddykopudo <teddykopudo@gmail.com>
2023-06-26 14:17:45 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * reset_to_98 - resets the value it points to 98 
+ * @n: the variable to be updated to 98
+ * Return: no return.
+ * */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
