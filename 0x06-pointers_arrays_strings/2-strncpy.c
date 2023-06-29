38baaeb6c2e2f29b423481f8a1de877196e4fc7b
teddykopudo <teddykopudo@gmail.com>
2023-06-29 14:39:12 -0700

    2. strcpy

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncpy - copy a string
+ * @dest: input value
+ * @src: input value
+ * @n: input value
+ * Return: dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int t;
+
+	t = 0;
+	while (t < n && src[t] != '\0')
+	{
+		dest[t] = src[t];
+		t++;
+	}
+	while (t < n)
+	{
+		dest[t] = '\0';
+		t++;
+	}
+	return (dest);
+}
