39f51615b39a80db22b498f0a9459a2cb393fa46
teddykopudo <teddykopudo@gmail.com>
2023-06-29 17:02:18 -0700

    print_buffer

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -17,31 +17,35 @@ void print_buffer(char *b, int size)
 		printf("\n");
 		return;
 	}
+
 	while (t < size)
 	{
-		k = size - t < 10 ? size - t : 10;
+		k = (size - t < 10) ? (size - t) : 10;
 		printf("%08x: ", t);
+
 		for (i = 0; i < 10; i++)
 		{
 			if (i < k)
 				printf("%02x", *(b + t + i));
 			else
 				printf("  ");
+
 			if (i % 2)
-			{
 				printf(" ");
-			}
 		}
+
 		for (i = 0; i < k; i++)
 		{
 			int c = *(b + t + i);
 
-			if (c < 32 || c > 132)
-			{
+			if (c < 32 || c > 126)
 				c = '.';
-			}
-			printf("\n");
-			t += 10;
+
+			printf("%c", c);
 		}
+
+		printf("\n");
+		t += 10;
 	}
 }
+
