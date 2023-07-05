118d9a14db3778995c1c107b580564d344ce0cba
teddykopudo <teddykopudo@gmail.com>
2023-07-05 13:59:37 -0700

    0-puts_recursion

---
+++ 0x08-recursion/_putchar.c
@@ -0,0 +1,13 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: on success 1
+ * on error, -1 is returned, errno set accordingly
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -1,5 +1,6 @@
 #ifndef _MAIN_H
 #define _MAIN_H
+
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
@@ -7,4 +8,6 @@ int factorial(int n);
 int _pow_recursion(int x, int y);
 int _sqrt_recursion(int n);
 int is_prime_number(int n);
+int _putchar(char c);
+int is_palindrome(char *s);
 #endif
