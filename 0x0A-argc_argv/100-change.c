f2b0c2eb268ba62adde7e9a334249d65ad4740d8
teddykopudo <teddykopudo@gmail.com>
2023-07-07 16:44:04 -0700

    change

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -19,7 +19,7 @@ int main(int argc, char *argv[])
 	if (argc != 2)
 	{
 		printf("Error\n");
-		return 1;
+		return (1);
 	}
 
 	cents = atoi(argv[1]);
@@ -27,18 +27,18 @@ int main(int argc, char *argv[])
 	if (cents < 0)
 	{
 		printf("0\n");
-		return 0;
+		return (0);
 	}
 
 		for (i = 0; i < num_coin_types; i++)
 	{
 		while (cents >= coins[i])
-        {
+	{
 		cents -= coins[i];
 		num_coins++;
 	}
 	}
 
 		printf("%d\n", num_coins);
-		return 0;
+		return (0);
 }
