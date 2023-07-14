cea32b6c22ff120b84699a991e760bbd882e65f9
teddykopudo <teddykopudo@gmail.com>
2023-07-13 18:29:22 -0700

    mul

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -20,9 +20,14 @@ int main(int argc, char *argv[])
 	num1 = argv[1];
 	num2 = argv[2];
 
-	for (i = 0; num1[i] != '\0'; i++)
+	for (i = 0; num1[i] != '\0' && num2[i] != '\0'; i++)
 {
 	if (!isdigit(num1[i]) || !isdigit(num2[i]))
+{
+	printf("Error\n");
+	return (98);
+}
+	if (num1[i] != num2[i])
 {
 	printf("Error\n");
 	return (98);
