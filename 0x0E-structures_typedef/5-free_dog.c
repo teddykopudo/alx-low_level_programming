0bc17b8e7d21789d3aad1cf45b8fa79672ccf275
teddykopudo <teddykopudo@gmail.com>
2023-07-17 14:43:05 -0700

    free

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdlib.h>
+/**
+ * free_dog - free
+ *
+ * @d: struct
+ *
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	if (d == NULL)
+	return;
+
+	free(d->owner);
+	free(d->name);
+	free(d);
+}
