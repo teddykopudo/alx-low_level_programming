5ae22d644c05af554274a7c1b45254c64b4066f6
teddykopudo <teddykopudo@gmail.com>
2023-08-01 18:56:41 -0700

    free list2

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -4,10 +4,13 @@
  * free_listint2 - Frees a linked list and sets the head to NULL.
  * @head: Double pointer to the head of the list.
  *
- * Return: No return.
+ * Return: void
  */
 void free_listint2(listint_t **head)
 {
+	if (head == NULL || *head == NULL)
+		return;
+
 	listint_t *current = *head;
 	listint_t *next;
 
